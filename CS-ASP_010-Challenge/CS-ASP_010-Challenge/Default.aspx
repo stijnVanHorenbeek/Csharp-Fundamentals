1612dc20c2b82eacc574ea0e016fed84cca45108
stijnVanHorenbeek <stijn.van.horenbeek@outlook.com>
2017-05-18 15:44:43 +0200

    Lesson 2 - Data Types, Variables and Operators
    Lesson 2 - Data Types, Variables and Operators

---
+++ CS-ASP_010-Challenge/CS-ASP_010-Challenge/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_010_Challenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+            <span class="auto-style2">First Value:&nbsp; </span>
+            <asp:TextBox ID="firstNumberTextBox" runat="server" CssClass="auto-style2"></asp:TextBox>
+            <br class="auto-style2" />
+            <span class="auto-style2">Second Value:&nbsp; </span>
+            <asp:TextBox ID="secondNumberTextBox" runat="server" CssClass="auto-style2"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="Button1_Click" Text="+" />
+&nbsp;
+            <asp:Button ID="subtractionButton" runat="server" OnClick="subtractionButton_Click" Text="-" />
+&nbsp;
+            <asp:Button ID="multiButton" runat="server" OnClick="multiButton_Click" Text="*" />
+&nbsp;
+            <asp:Button ID="divisionButton" runat="server" OnClick="divisionButton_Click" Text="/" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" BackColor="#0099FF" Font-Bold="True" Font-Size="Larger" ForeColor="Black"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_010-Challenge/CS-ASP_010-Challenge/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_010_Challenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double firstNumber = double.Parse(firstNumberTextBox.Text);
+            double secondNumber = double.Parse(secondNumberTextBox.Text);
+            double result = firstNumber + secondNumber;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = double.Parse(firstNumberTextBox.Text);
+            double secondNumber = double.Parse(secondNumberTextBox.Text);
+            double result = firstNumber - secondNumber;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = double.Parse(firstNumberTextBox.Text);
+            double secondNumber = double.Parse(secondNumberTextBox.Text);
+            double result = firstNumber * secondNumber;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = double.Parse(firstNumberTextBox.Text);
+            double secondNumber = double.Parse(secondNumberTextBox.Text);
+            double result = firstNumber / secondNumber;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
